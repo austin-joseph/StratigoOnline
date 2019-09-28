594c89554d30870f1999b103720ae37a5655591c
Anil Ramsoomye <anilramsoomye@gmail.com>
2019-09-28 16:08:15 -0400

    database functionality

---
+++ src/main/java/darkpurple/hw1/entity/gameInfo.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package darkpurple.hw1.entity;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.*;
+
+/**
+ *
+ * @author anilramsoomye
+ */
+@Document (collection = "gameInfo")
+public class gameInfo {
+    @Id
+    private String gameID;
+    
+    private String player1; //AI so 0
+    private String player2; //email of player
+    
+    private String winner; //email
+    
+    
+    private LinkedList<pieceState> stateList;
+    private LinkedList<move> moves;
+        
+    
+    public class move{
+        private String startingSpot;
+        private String endingSpot;
+        private String piece;
+        private String pieceOwner;
+    }
+    
+    public class pieceState {
+        private String startingSpot;
+        private String endingSpot;
+        private String pieceOwner;
+                
+    }
+
+    public String getGameID() {
+        return gameID;
+    }
+
+    public String getPlayer1() {
+        return player1;
+    }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public LinkedList<pieceState> getStateList() {
+        return stateList;
+    }
+
+    public LinkedList<move> getMoves() {
+        return moves;
+    }
+
+    public void setGameID(String gameID) {
+        this.gameID = gameID;
+    }
+
+    public void setPlayer1(String player1) {
+        this.player1 = player1;
+    }
+
+    public void setPlayer2(String player2) {
+        this.player2 = player2;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public void setStateList(LinkedList<pieceState> stateList) {
+        this.stateList = stateList;
+    }
+
+    public void setMoves(LinkedList<move> moves) {
+        this.moves = moves;
+    }
+    
+    
+    
+}
