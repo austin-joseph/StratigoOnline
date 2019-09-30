228977fbc4619284f5efb8d9bd4516d673909b92
Anil Ramsoomye <anilramsoomye@gmail.com>
2019-09-30 18:49:10 -0400

    Update PageConfig.java

--- src/main/java/darkpurple/hw1/config/PageConfig.java
+++ src/main/java/darkpurple/hw1/config/PageConfig.java
@@ -1,6 +1,5 @@
 package darkpurple.hw1.config;
 
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         import org.springframework.context.annotation.Bean;
         import org.springframework.context.annotation.Configuration;
         import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
