0da35b9e982c1889af1949e8f8ca2cc943cf5e24
jkc7848 <jkc7848@gmail.com>
2019-10-01 23:32:41 -0400

    Updated Past Games
    All functionalities for View Paste Games completed.

--- src/main/resources/static/js/pastgames.js
+++ src/main/resources/static/js/pastgames.js
@@ -4,14 +4,34 @@
  * and open the template in the editor.
  */
 $(document).ready(function() {
+
     $.ajax({
         url: "/getgames",
         type: "POST",
         dataType: "json",
         success: function(data) {
             games = data;
-            console.log(JSON.parse(games[0].jsonBody));
+
+            for (var i = 0; i < games.length; i++) {
+                var date = new Date(games[i].date*1000);
+                var winner = JSON.parse(games[i].jsonBody).winner;
+                var userPiecesLost = JSON.parse(games[i].jsonBody).userPiecesLost;
+                var aiPiecesLost = JSON.parse(games[i].jsonBody).aiPiecesLost;
+
+                if (winner == 1) {
+                    $('#tbody-games-list').prepend('<tr><td>'+date+'</td><td>You Won</td></tr>');
+                }
+                else {
+                    $('#tbody-games-list').prepend('<tr><td>'+date+'</td><td>AI Won</td></tr>');
+                }
+
+                $('#tbody-lost-list').prepend('<tr><td style="color: #0062cc;">'+userPiecesLost+'</td><td style="color: red;">'+aiPiecesLost+'</td></tr>');
+
+            }
         }
     });
 
+
+
 });
+
